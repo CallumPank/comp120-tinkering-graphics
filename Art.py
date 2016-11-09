ef5e83bfa81f7c0233adabfffc515136404988ec
Callum Pankhurst <callpank97@gmail.com>
2016-11-09 01:20:52 +0000

    Nothing Changed

--- Art.py
+++ Art.py
@@ -4,27 +4,21 @@ from pygame.locals import *
 
 pygame.init()
 
+
 screen = pygame.display.set_mode((400, 585))
 image = pygame.image.load('Lute.jpg')
 
-sprite = pygame.sprite.Sprite()
-sprite.image = image
-sprite.rect = image.get_rect()
 
 font = pygame.font.SysFont('Sans', 58)
-text = font.render("Here's Wonderwall", True, (255, 255, 255))
+text = font.render("Anyway", True, (255, 255, 255))
+text2 = font.render("Here's Wonderwall", True, (255, 255, 255))
+screen.blit(image, (0, 0))
+screen.blit(text, (115, 0))
+screen.blit(text2, (0, 500))
 
-sprite.image.blit(text, sprite.rect)
-
-group = pygame.sprite.Group()
-group.add(sprite)
-group.draw(screen)
 
 pygame.display.flip()
 
-print 'bits per pixel:'
-print 'image', image.get_bitsize()
-print 'screen', screen.get_bitsize()
 
 while True:
     for event in pygame.event.get():
