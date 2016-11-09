3fe45c5352e4ad898a0b4712fc0736f403384d3d
Callum Pankhurst <callpank97@gmail.com>
2016-11-09 01:20:38 +0000

    Nothing Changed

--- Jesus.py
+++ Jesus.py
@@ -1,28 +1,20 @@
 import pygame
 import sys
-import time
 from pygame.locals import *
 
 pygame.init()
 
-Width = 400
-Height = 585
-
-BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode((400, 585))
 image = pygame.image.load('Jesus.jpg')
 
-a = 100
-b = 200
-
 
 font = pygame.font.SysFont('Sans', 38)
 text = font.render("When Your Phone Runs Out", True, (255, 255, 255))
-text2 = font.render("In a Lecture", True, (255, 255, 255))
+text2 = font.render("In A Three Hour Lecture", True, (255, 255, 255))
 screen.blit(image, (0, 0))
-screen.blit(text,(0, 0))
-screen.blit(text2,(115, 500))
+screen.blit(text, (0, 0))
+screen.blit(text2, (39, 500))
 
 
 pygame.display.flip()
